b12064d3db871a8bed2c10a28a4417babbc46fd8
Michael Ishmael <michaelishmael1976@gmail.com>
2016-01-28 23:55:28 +0000

    More UI work

--- proudction-ui/app/controllers/imgProcController.js
+++ proudction-ui/app/controllers/imgProcController.js
@@ -4,8 +4,55 @@
 
 app.controller('imgProcController', ['$scope', function ($scope) {
 
+    $scope.selectedImg = null;
+
     $scope.backlog = data.backlog;
 
+    var jcrop_api;
+
+    initJcrop();
+
+    function initJcrop()//{{{
+    {
+
+
+
+    }
+
+    $('#cropbox').load(function(){
+
+        jcrop_api = $.Jcrop('#cropbox');
+        jcrop_api.setOptions({ allowResize: false });
+
+    });
+
+    $scope.preImageChange = function(){
+        if(jcrop_api){
+            jcrop_api.destroy();
+        }
+    };
+
+    $scope.getImagePath = function(){
+
+        if(!$scope.selectedImg){
+            return "placeholder.gif";
+        }
+        return "../media/" + $scope.selectedImg[0].path;
+    };
+
+    $scope.addCropBox = function(src){
+
+        switch (src)
+        {
+            case 1: // twelve16
+                jcrop_api.setSelect([10, 10, 20, 20]);
+                break;
+            case 2: // nine16
+                jcrop_api.setSelect([10, 10, 40, 40]);
+                break;
+        }
+
+    }
 
 
 
@@ -17,9 +64,9 @@ app.controller('imgProcController', ['$scope', function ($scope) {
 var data = {
     backlog: [
         {id: 1, path:"donkey1.jpg"},
-        {id: 2, path:"crit1.png"},
-        {id: 3, path:"crit2.png"}
-
+        {id: 2, path:"cow3.jpg"},
+        {id: 3, path:"crit1.png"},
+        {id: 4, path:"crit2.png"}
     ],
     imageItems: [
         {
