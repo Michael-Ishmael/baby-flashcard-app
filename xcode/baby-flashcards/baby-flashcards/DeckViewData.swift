8bd91ae634e46740df9dda2ae1eb6fdecfb8b731
Michael Ishmael <michaelishmael1976@gmail.com>
2016-05-16 07:49:07 +0100

    Swift transfer 1

---
+++ xcode/baby-flashcards/baby-flashcards/DeckViewData.swift
@@ -0,0 +1,9 @@
+//
+//  DeckViewData.swift
+//  baby-flashcards
+//
+//  Created by Michael Ishmael on 13/05/2016.
+//  Copyright © 2016 66Bytes. All rights reserved.
+//
+
+import Foundation
---
+++ xcode/baby-flashcards/baby-flashcards/FlashCardViewController.swift
@@ -0,0 +1,9 @@
+//
+//  FlashCardViewController.swift
+//  baby-flashcards
+//
+//  Created by Michael Ishmael on 14/05/2016.
+//  Copyright © 2016 66Bytes. All rights reserved.
+//
+
+import Foundation
